462f6ba6700eca7c6d934a225c7a570a8d99d449
Sachinyaduvanshi7 <sachinyrao7@gmail.com>
2024-10-10 10:36:39 +0530

    11

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3002;
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Message from product service-45");
+  res.send("Message from product service");
 });
 
 app.listen(PORT, () => console.log("PRODUCT SERVICE STARTED"));
